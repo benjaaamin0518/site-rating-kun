f05b1af2beb05805e2f6eaade348212e440fe085
nishikawa <nishikawa@benest.co.jp>
2022-01-21 15:16:18 +0900

    twitter等のSPAサイトに対応した形に動作を変更した

--- content_script.js
+++ content_script.js
@@ -4,6 +4,7 @@ let uuu;
 let _select;
 let r_length;
 
+// alert(`updated:`);
 
 
 // content_script.js
@@ -23,6 +24,7 @@ async function useURLFunc() {
 (async () => {
     await useURLFunc();
 })();
+
 chrome.storage.sync.get(['date'], function (value) {
     value=value.date||[];
     console.log(value);
@@ -88,3 +90,112 @@ chrome.storage.sync.get(['date'], function (value) {
         }
     } 
  
+    var href = location.href;
+    var observer = new MutationObserver(function(mutations) {
+      if(href !== location.href) {
+        let _date;
+let _url;
+let uuu;
+let _select;
+let r_length;
+
+// alert(`updated:`);
+
+
+// content_script.js
+async function useURLFunc() {
+    let sendMsgFunc = () => {
+        return new Promise(resolve => {
+            chrome.runtime.sendMessage({ greeting: 'url' }, response => {
+                resolve(response.farewell);
+            });
+        });
+    };
+    let url = await sendMsgFunc();
+    uuu=url;
+
+}
+
+(async () => {
+    await useURLFunc();
+})();
+
+chrome.storage.sync.get(['date'], function (value) {
+    value=value.date||[];
+    console.log(value);
+    _date=value;
+  });
+  chrome.storage.sync.get(['select'], function (value) {
+    value=value.select||[];
+    console.log(value);
+    _select=value;
+
+   });
+       chrome.storage.sync.get(['url'], function (value) {
+    value=value.url||[];
+    console.log(value);
+    _url=value;
+    r_length=_url.length;
+   });
+   let set_interval_id2 = setInterval(findTargetElement2, 300);
+   function findTargetElement2(){
+
+
+
+
+
+
+        if(r_length!=='undefined'){
+            clearInterval(set_interval_id2);
+            bool=_url.indexOf(uuu);
+            if(bool==-1){
+                if(document.getElementsByClassName('box23')){
+                    $('.box23').remove();
+                }                if(document.getElementsByClassName('box22')){
+                    $('.box22').remove();
+                }
+                $('body').prepend(
+`<center><div class="box23">
+<p>このサイトはまだ評価がありません</p>
+
+</div></center>
+
+`
+                );
+            }
+            else{
+                let vx="";
+                switch(_select[bool]){
+                    case '0':vx="とてもいい";break;
+                    case '1':vx="いい";break;
+                    case '2':vx="わっるい";break;
+                    case '3':vx="とてもわっるい";break;
+                }
+                if(document.getElementsByClassName('box23')){
+                    $('.box23').remove();
+                }                if(document.getElementsByClassName('box22')){
+                    $('.box22').remove();
+                }
+                $('body').prepend(
+                    `<center><div class="box22">
+                    <p>このサイトの評価は<b>${vx}</b>です</p>
+                    
+                    </div></center>
+                    
+                    `
+                                    ); 
+            }
+    
+        }
+        else{
+    
+    
+            
+    
+        }
+    } 
+    href=location.href;
+           }
+    });
+    
+    observer.observe(document, { childList: true, subtree: true });
