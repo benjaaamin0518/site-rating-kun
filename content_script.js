72b1b10835d51b4b3dfb19b52e76ee90c7a00fd3
nishikawa <nishikawa@benest.co.jp>
2022-08-10 10:30:01 +0900

    少しスクロール処理を高速化にした。

--- content_script.js
+++ content_script.js
@@ -423,17 +423,22 @@ var href = location.href;
 let obsbool = false;
 let copy;
 let test = () => {
-  if (_url) {
-    var bool = false;
-    _url.forEach(element => {
-      var urlStr = element;
-      var result = (getRootRelative(urlStr) == "/" || (getRootRelative(urlStr) == "")) ? false : getRootRelative(urlStr);
-      if ($(`a[href='${result}']`).text().length) { bool = true }
-    })
-    return bool;
-  } else {
-    return false;
-  }
+  return new Promise((resolve, reject) => {
+    if (_url) {
+      var bool = false;
+      _url.forEach(element => {
+        var urlStr = element;
+        var result = (getRootRelative(urlStr) == "/" || (getRootRelative(urlStr) == "")) ? false : getRootRelative(urlStr);
+        // console.log(result);
+
+        if ($(`a[href='${result}']`).text().length) { bool = true; resolve(bool) } else if ($(`a[href='${element}']`).text().length) { bool = true; resolve(bool) }
+      })
+      console.log(bool);
+      resolve(bool);
+    } else {
+      resolve(false);
+    }
+  })
 };
 let obs = (mutations) => {
   return new Promise((resolve, reject) => {
@@ -451,11 +456,14 @@ var observer = new MutationObserver(async function (mutations) {
   // let added = mutations.push(e => { return e });
   // // console.log(added.MutationRecord.addedNodes);
   // console.table(added);
-  obsbool = true;
-  obsbool = await obs(mutations);
-  obsbool = (obsbool) ? test() : false;
-  console.log(obsbool);
-  if (obsbool) {
+  if (href == location.href) {
+    console.log(obsbool);
+
+    obsbool = await obs(mutations);
+    obsbool = (obsbool) ? await test() : false;
+    console.log(obsbool);
+  }
+  if (obsbool && href == location.href) {
     count++;
     if (href !== location.href) {
       let _date;
@@ -688,7 +696,250 @@ var observer = new MutationObserver(async function (mutations) {
     href = location.href;
   }
 });
-observer.observe(document, {
+var observerDif = new MutationObserver(async function (mutations) {
+  // (mutations.addedNodes == "div.box23") ? console.log("おったわ") : "";
+  // let added = mutations.push(e => { return e });
+  // // console.log(added.MutationRecord.addedNodes);
+  // console.table(added);
+  if (href !== location.href) {
+    obsbool = await obs(mutations);
+    obsbool = (obsbool) ? await test() : false;
+    console.log(obsbool);
+  }
+  if (href !== location.href) {
+    count++;
+    if (href !== location.href) {
+      let _date;
+      let _url;
+      let _select;
+      let r_length;
+    }
+    kai = 0;
+    // alert(`updated:`);
+    // content_script.js
+    if (href !== location.href) {
+      async function useURLFunc() {
+        let sendMsgFunc = () => {
+          return new Promise(resolve => {
+            chrome.runtime.sendMessage({
+              greeting: 'url'
+            }, response => {
+              resolve([response.farewell, response.title]);
+            });
+          });
+        };
+        let url = await sendMsgFunc();
+        uuu = url[0];
+        title = url[1]
+      }
+      (async () => {
+        await useURLFunc();
+      })();
+      chrome.storage.local.get(['date'], function (value) {
+        value = value.date || [];
+        console.log(value);
+        _date = value;
+      });
+      chrome.storage.local.get(['select'], function (value) {
+        value = value.select || [];
+        console.log(value);
+        _select = value;
+      });
+      chrome.storage.local.get(['url'], function (value) {
+        value = value.url || [];
+        console.log(value);
+        _url = value;
+        r_length = _url.length;
+      });
+    }
+    let set_interval_id2 = setInterval(findTargetElement2, 50);
+
+    function findTargetElement2() {
+      if (r_length !== 'undefined') {
+        clearInterval(set_interval_id2);
+        bool = _url.indexOf(uuu);
+        if (bool == -1) {
+          if (document.getElementsByClassName('box23')) {
+            $('.box23').remove();
+          }
+          if (document.getElementsByClassName('box22')) {
+            $('.box22').remove();
+          }
+          if (document.getElementsByClassName('site_evaluation')) {
+            $('.site_evaluation').remove();
+          }
+          let html_pre = "";
+          _url.forEach(element => {
+            var url_bool = _url.indexOf(element);
+            var urlStr = element;
+            var result = (getRootRelative(urlStr) == "/" || (getRootRelative(urlStr) == "")) ? false : getRootRelative(urlStr);
+            var select_vx = "";
+            switch (_select[url_bool]) {
+              case '0':
+                select_vx = "とてもいい";
+                break;
+              case '1':
+                select_vx = "いい";
+                break;
+              case '2':
+                select_vx = "わっるい";
+                break;
+              case '3':
+                select_vx = "とてもわっるい";
+                break;
+            }
+            html_pre = `<span class='site_evaluation' style='  
+                    box-sizing: border-box;
+                    white-space: nowrap;
+                    border-radius: 0.2em;
+                    padding: 0.12em 0.4em 0.14em;
+                    margin: 0 0.42em;
+                    text-align: center;
+                    color: #ffffff;
+                    font-size: 0.75em;
+                    font-weight: 700;
+                    line-height: 1;
+                    display: inline;
+                    white-space: nowrap;
+                    vertical-align: baseline;
+                    position: relative;
+                    top: -0.15em;
+                    background-color: #9bc268;'>評価:${select_vx}</span>`;
+            let bb = (result) ? result : console.log(element);
+            let han = ($(`a[href='${result}']`).text().length) ? $(`a[href='${result}']`).prepend(html_pre) : $(`a[href='${element}']`).prepend(html_pre);
+          });
+          if ($('.qiita_link .site_evaluation').text()) {
+            $('.qiita_link .site_evaluation').remove();
+          }
+          $('body').prepend(`
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+            <center><div class="box23">
+            <p>このサイトはまだ評価がありません
+            <a href="https://twitter.com/search?q=url:${uuu}&src=typed_query&f=top" target="_blank"  class="text-decoration-none" id="tw_ter">
+            URLを検索
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+            <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path>
+            </svg>
+            </a>
+            </p>
+            </div>
+
+            </center>
+            `);
+        } else {
+          let vx = "";
+          switch (_select[bool]) {
+            case '0':
+              vx = "とてもいい";
+              break;
+            case '1':
+              vx = "いい";
+              break;
+            case '2':
+              vx = "わっるい";
+              break;
+            case '3':
+              vx = "とてもわっるい";
+              break;
+          }
+          //日付オブジェクトを作成する
+          let dd = new Date();
+          //「年」を取得する
+          let YYYY = dd.getFullYear();
+          //「月」を取得する
+          let MM = dd.getMonth() + 1;
+          let DD = dd.getDate();
+          let year = YYYY - 3;
+          let limit = new Date(year + "/" + MM + "/" + DD);
+          let date;
+          date = new Date(_date[bool]);
+          let date_flg = 0;
+          if (limit.getTime() >= date.getTime()) {
+            date_flg = 1;
+          }
+          let _year = "";
+          if (date_flg > 0) {
+            _year = "<br><u>このサイトは評価をつけてから3年以上経過しており情報が古い可能性があります。</u>"
+          }
+          if (document.getElementsByClassName('box23')) {
+            $('.box23').remove();
+          }
+          if (document.getElementsByClassName('box22')) {
+            $('.box22').remove();
+          }
+          if (document.getElementsByClassName('site_evaluation')) {
+            $('.site_evaluation').remove();
+          }
+          let html_pre = "";
+          _url.forEach(element => {
+            var url_bool = _url.indexOf(element);
+            var urlStr = element;
+            var result = (getRootRelative(urlStr) == "/" || (getRootRelative(urlStr) == "")) ? false : getRootRelative(urlStr);
+            var select_vx = "";
+            switch (_select[url_bool]) {
+              case '0':
+                select_vx = "とてもいい";
+                break;
+              case '1':
+                select_vx = "いい";
+                break;
+              case '2':
+                select_vx = "わっるい";
+                break;
+              case '3':
+                select_vx = "とてもわっるい";
+                break;
+            }
+            html_pre = `<span class='site_evaluation' style='  
+                    box-sizing: border-box;
+                    white-space: nowrap;
+                    border-radius: 0.2em;
+                    padding: 0.12em 0.4em 0.14em;
+                    margin: 0 0.42em;
+                    text-align: center;
+                    color: #ffffff;
+                    font-size: 0.75em;
+                    font-weight: 700;
+                    line-height: 1;
+                    display: inline;
+                    white-space: nowrap;
+                    vertical-align: baseline;
+                    position: relative;
+                    top: -0.15em;
+                    background-color: #9bc268;'>評価:${select_vx}</span>`;
+            let bb = (result) ? result : console.log(element);
+            let han = ($(`a[href='${result}']`).text().length) ? $(`a[href='${result}']`).prepend(html_pre) : $(`a[href='${element}']`).prepend(html_pre);
+          });
+          if ($('.qiita_link .site_evaluation').text()) {
+            $('.qiita_link .site_evaluation').remove();
+          }
+          $('body').prepend(`
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+                    <center><div class="box22">
+                    <p>このサイトの評価は<b>${vx}</b>です${_year}
+                    <a href="https://twitter.com/search?q=url:${uuu}&src=typed_query&f=top" target="_blank"  class="text-decoration-none" id="tw_ter">
+                    URLを検索
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path>
+                    </svg>
+                    </a>
+                    </p>
+
+                    </div>
+
+
+
+
+                    </center>
+
+                    `);
+        }
+      } else { }
+    }
+    href = location.href;
+  }
+});
+observerDif.observe(document, {
   childList: true,
   subtree: true
 });
@@ -696,6 +947,12 @@ const bodyHeight = document.body.clientHeight // bodyの高さを取得
 const windowHeight = window.innerHeight // windowの高さを取得
 const bottomPoint = bodyHeight - windowHeight // ページ最下部までスクロールしたかを判定するための位置を計算
 window.addEventListener('scroll', () => {
+  obsbool = true;
+
+  observer.observe(document, {
+    childList: true,
+    subtree: true
+  });
   const currentPos = window.pageYOffset // スクロール量を取得
   let ui = "";
   if (bottomPoint <= currentPos && kai == 0) { // スクロール量が最下部の位置を過ぎたかどうか
